98539c5fddae3fd796217b28e4edcb6fde84387b
LiamVrankenPXL <Liam.Vranken2@student.pxl.be>
2023-06-09 22:29:34 +0200

    werken aan de cards in js

--- script_index.js
+++ script_index.js
@@ -32,3 +32,107 @@ window.addEventListener('scroll', () => {
 
     border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
 })
+
+
+
+
+// Sample data for the cards (replace with your own data)
+const projects = [
+    {
+        title: "Project 1",
+        category: "Photoshop",
+        image: "assets/Photoshop__2.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        title: "Project 2",
+        category: "Photoshop",
+        image: "assets/Photoshop__6.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        title: "Project 2",
+        category: "XD",
+        image: "assets/Photoshop__3.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        title: "Project 2",
+        category: "XD",
+        image: "assets/Photoshop__3.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        title: "Project 2",
+        category: "XD",
+        image: "assets/Photoshop__3.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        title: "Project 2",
+        category: "XD",
+        image: "assets/Photoshop__3.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        title: "Project 2",
+        category: "XD",
+        image: "assets/Photoshop__3.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    // Add more projects here
+];
+
+// Function to generate the card HTML
+function generateCard(project) {
+    return `
+        <div class="col-lg-2 col-md-4 col-sm-6 py-3 mx-1">
+            <div class="card">
+                <img src="${project.image}" class="card-img-top" alt="Project Image">
+                <div class="card-body">
+                    <h5 class="card-title">${project.title}</h5>
+                    <p class="card-text">${project.description}</p>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+
+// Function to display the filtered projects
+function displayProjects(projectsArray) {
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+
+    projectsArray.forEach((project) => {
+        const cardHTML = generateCard(project);
+        cardContainer.innerHTML += cardHTML;
+    });
+}
+
+// Function to filter projects by category
+function filterProjects(category) {
+    if (category === "all") {
+        displayProjects(projects);
+    } else {
+        const filteredProjects = projects.filter((project) => project.category === category);
+        displayProjects(filteredProjects);
+    }
+}
+
+// Event listener for category badges
+const categoryBadges = document.getElementsByClassName("categoria_badge");
+Array.from(categoryBadges).forEach((badge) => {
+    badge.addEventListener("click", () => {
+        const category = badge.getAttribute("data-categoria");
+        filterProjects(category);
+        // Add 'active' class to the clicked badge and remove from others
+        Array.from(categoryBadges).forEach((b) => {
+            b.classList.remove("active");
+        });
+        badge.classList.add("active");
+    });
+});
+
+// Initial display of all projects
+displayProjects(projects);
